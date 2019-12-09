924503daf952e6ac684e058dfec2149b8a337a44
navyars <navyars82@gmail.com>
2019-12-09 16:41:40 +0530

    Fix tracing warning
    Earlier, we called tf functions with python lists. This invoked
    TF's retracing many times, and a warning message was consistently
    displayed. This commit fixes that by using tf constant for every
    input, thus speeding up the training significantly.

--- ppo.py
+++ ppo.py
@@ -14,22 +14,31 @@ class PPO:
         self.learning_rate = learning_rate
         
         self.weights = []
-        create_variable = lambda shape : tf.Variable(tf.random.normal(shape=shape, dtype=tf.double))
-        # feature_layer
-        self.common_kernel = create_variable((input_shape, 64))
-        self.common_bias = create_variable((64,))
+        create_variable = lambda shape : tf.Variable(tf.random.normal(shape=shape, dtype=tf.float32))
+        # # feature_layer
+        # self.common_kernel = create_variable((input_shape, 64))
+        # self.common_bias = create_variable((64,))
+        # # pi
+        # self.pi_kernel = [create_variable((64, 64)), create_variable((64, num_actions))]
+        # self.pi_bias = [create_variable((64,))]
+        # # value
+        # self.value_kernel = [create_variable((64, 64)), create_variable((64, 1))]
+        # self.value_bias = [create_variable((64,))]
+
+        self.common_kernel = create_variable((input_shape, 256))
+        self.common_bias = create_variable((256,))
         # pi
-        self.pi_kernel = [create_variable((64, 64)), create_variable((64, num_actions))]
-        self.pi_bias = [create_variable((64,))]
+        self.pi_kernel = [create_variable((256, num_actions))]
+        # self.pi_bias = [create_variable((,))]
         # value
-        self.value_kernel = [create_variable((64, 64)), create_variable((64, 1))]
-        self.value_bias = [create_variable((64,))]
+        self.value_kernel = [create_variable((256, 1))]
+        # self.value_bias = [create_variable((64,))]
         self.weights = []
         self.weights.extend([self.common_kernel, self.common_bias])
         self.weights.extend(self.pi_kernel)
-        self.weights.extend(self.pi_bias)
+        # self.weights.extend(self.pi_bias)
         self.weights.extend(self.value_kernel)
-        self.weights.extend(self.value_bias)
+        # self.weights.extend(self.value_bias)
 
         self.optimizer = tf.keras.optimizers.Adam(learning_rate)
 
@@ -43,13 +52,13 @@ class PPO:
         bias_layer = tf.nn.bias_add(feature_layer, self.common_bias)
         activation_layer = tf.nn.relu(bias_layer)
 
-        feature_layer_2 = tf.matmul(activation_layer, self.pi_kernel[0])
-        bias_layer_2 = tf.nn.bias_add(feature_layer_2, self.pi_bias[0])
-        activation_layer_2 = tf.nn.relu(bias_layer_2)
+        # feature_layer_2 = tf.matmul(activation_layer, self.pi_kernel[0])
+        # bias_layer_2 = tf.nn.bias_add(feature_layer_2, self.pi_bias[0])
+        # activation_layer_2 = tf.nn.relu(bias_layer_2)
 
-        feature_layer_3 = tf.matmul(activation_layer_2, self.pi_kernel[1])
-        sigmoid_pi = tf.nn.sigmoid(feature_layer_3)
-        return sigmoid_pi
+        feature_layer_3 = tf.matmul(activation_layer, self.pi_kernel[0])
+        softmax_pi = tf.nn.softmax(feature_layer_3)
+        return softmax_pi
 
     @tf.function
     def value(self, inp):
@@ -57,11 +66,11 @@ class PPO:
         bias_layer = tf.nn.bias_add(feature_layer, self.common_bias)
         activation_layer = tf.nn.relu(bias_layer)
 
-        feature_layer_2 = tf.matmul(activation_layer, self.value_kernel[0])
-        bias_layer_2 = tf.nn.bias_add(feature_layer_2, self.value_bias[0])
-        activation_layer_2 = tf.nn.relu(bias_layer_2)
+        # feature_layer_2 = tf.matmul(activation_layer, self.value_kernel[0])
+        # bias_layer_2 = tf.nn.bias_add(feature_layer_2, self.value_bias[0])
+        # activation_layer_2 = tf.nn.relu(bias_layer_2)
 
-        fc_v = tf.matmul(activation_layer_2, self.value_kernel[1])
+        fc_v = tf.matmul(activation_layer, self.value_kernel[0])
         return fc_v
     
     @tf.function
@@ -89,8 +98,8 @@ class PPO:
         self.experience_buffer = [0] * len(self.history)
         for i in range(len(self.history)-1, -1, -1):
             s, a, s_prime, r, prob_action = self.history[i]
-            target = r + self.gamma * self.value(np.expand_dims(s_prime, 0)).numpy()[0]
-            td_error = target - self.value(np.expand_dims(s, 0)).numpy()[0]
+            target = r + self.gamma * self.value(tf.constant(np.expand_dims(s_prime, 0),dtype=tf.float32)).numpy()[0]
+            td_error = target - self.value(tf.constant(np.expand_dims(s, 0),dtype=tf.float32)).numpy()[0]
             advantage = td_error + self.gamma * self.lambd * advantage
             self.experience_buffer[i] = (s, a, prob_action, target, advantage)
         return
@@ -105,36 +114,46 @@ class PPO:
             train_batch_indices = np.random.choice(range(len(self.experience_buffer)), self.batch_size)
             train_batch = [self.experience_buffer[i] for i in train_batch_indices]
             nth_list = lambda i : [el[i] for el in train_batch]
+            # create_tensor = lambda x : tf.constant(x, dtype=tf.float32)
             s_list, a_list, prob_action_list, target_list, advantage_list = map(nth_list, range(5))
+            s_tensor = tf.constant(s_list, dtype=tf.float32)
+            a_tensor = tf.constant(a_list, dtype=tf.int32)
+            prob_action_tensor = tf.constant(prob_action_list, dtype=tf.float32)
+            target_tensor = tf.constant(target_list, dtype = tf.float32)
+            advantage_tensor = tf.constant(advantage_list, dtype=tf.float32)
 
-            self.train_single_step(s_list, a_list, prob_action_list, target_list, advantage_list)
+            self.train_single_step(s_tensor, a_tensor, prob_action_tensor, target_tensor, advantage_tensor)
 
 
 def generate_trajectory(env, model, start_state, horizon):
     s = start_state
     done = False
     history = []
+    score = 0
     for _ in range(horizon):
-        prob_action = model.pi(np.expand_dims(s,0)).numpy()[0]
+        prob_action = model.pi(tf.constant(np.expand_dims(s,0),dtype=tf.float32)).numpy()[0]
         a = np.argmax(prob_action)
         #TODO: env.step
         s_prime, reward, done, _ = env.step(a)
-        env.render()
+        # env.render()
         history.append((s, a, s_prime, reward, prob_action))
         s = s_prime
+        score += reward
         if done:
             break
-    return history
+    return history, score
 
 def collect_data(env, num_actors, horizon, model):
     history_buffer = []
+    avg_score = 0
     for actor in range(num_actors):
         #TODO: env.get_start_state
         # start_state = env.get_start_state()
         start_state = env.reset()
-        history = generate_trajectory(env, model, start_state, horizon)
+        history, score = generate_trajectory(env, model, start_state, horizon)
         history_buffer.extend(history)
-    return history_buffer
+        avg_score += score
+    return history_buffer, avg_score / num_actors
 
 # def permissible_action(inp)
 
--- testing_with_gym.py
+++ testing_with_gym.py
@@ -5,13 +5,13 @@ import numpy as np
 if __name__ == "__main__":
     env=gym.make("CartPole-v1")
     s=env.reset()
-    model=ppo.PPO(s.shape[0],2)
+    model=ppo.PPO(s.shape[0],2, learning_rate=5e-4, gamma=0.98, lambd=0.95, epsilon=0.1, num_epochs=3, batch_size=256)
     iters=1000
     for i in range(iters):
         s=env.reset()
-        history=ppo.collect_data(env, 20, 20, model)
-        reward = [ r for (_, _, _, r, _) in history]
-        reward=np.average(reward)
+        history, reward=ppo.collect_data(env, 20, 20, model)
+        # reward = [ r for (_, _, _, r, _) in history]
+        # reward=np.average(reward)
         model.populate_history(history)
         print("# of iters:{},avg_score:{}".format(i,reward))
         model.train()
