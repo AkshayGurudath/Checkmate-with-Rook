e8a444596ad2d6d4c26c3b2a5752fe6a971464ee
AkshayGurudath <49364092+AkshayGurudath@users.noreply.github.com>
2019-12-14 15:44:19 +0530

    Create chess_env.py
    The skeletal class for the Environment having roughly all the functions needed.

---
+++ chess_env.py
@@ -0,0 +1,36 @@
+import chess
+
+class ChessEnv:
+
+    def __init__(self):
+        pass
+
+    def _state_to_uci(self):
+        pass
+
+    def _uci_to_state(self):
+        pass
+
+    def _push_move(self):
+        pass
+
+    def _get_reward(self):
+        pass
+
+    def _get_current_state(self):
+        pass
+
+    def _episode_ended(self):
+        pass
+
+    def _legal_moves(self):
+        pass
+
+    def reset(self):
+        pass
+
+    def step(self):
+        pass
+
+    def render(self):
+        pass
