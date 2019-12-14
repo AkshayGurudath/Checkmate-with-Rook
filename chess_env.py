f16914c90e49b61de78c5efad3fb078ded6a4004
navyars <navyars82@gmail.com>
2019-12-14 17:32:51 +0530

    add get_current_state function
    - input: board
    - output: the state list in the form of (x, y) co-ordinates of the agent-king, agent-rook, and the opponent-king

--- chess_env.py
+++ chess_env.py
@@ -1,4 +1,5 @@
 import chess
+import numpy as np
 
 class ChessEnv:
 
@@ -17,8 +18,23 @@ class ChessEnv:
     def _get_reward(self):
         pass
 
-    def _get_current_state(self):
-        pass
+    def _get_current_state(self, board):
+        state = []
+        pieces = []
+        piece_map = board.piece_map()
+        inv_piece_map = {val: key for key, val in piece_map.items()}
+        if self.player_color == chess.WHITE:
+            pieces.extend(['K', 'R', 'k'])
+        else:
+            pieces.extend(['k', 'r', 'K'])
+        
+        for piece in pieces:
+            position = inv_piece_map.get(chess.Piece.from_symbol(piece), "")
+            if position:
+                state.append(( position // 8 ) + 1)  # x co-ordinate
+                state.append(( position % 8 ) + 1)  # y co-ordinate
+
+        return np.asarray(state)
 
     def _episode_ended(self):
         pass
